036e391dd97c9b63d7fcd16dbce45f93ec635858
Quack Quack <vivaladaman@gmail.com>
2021-10-21 15:17:21 +0700

    .

--- tcp-client/src/main.rs
+++ tcp-client/src/main.rs
@@ -1,16 +1,30 @@
 use std::{
-    io::{stdin, Read, Write},
+    io::{stdin, BufRead, BufReader, BufWriter, Read, Write},
     net::TcpStream,
+    thread,
 };
 
 fn main() {
-    let mut tcp_stream = TcpStream::connect("localhost:8080").unwrap();
-    loop {
-        let mut buffer = vec![0; 1024];
-        stdin()
-            .read(&mut buffer)
-            .expect("Can't seem to read from stdin");
+    let tcp_stream =
+        TcpStream::connect("localhost:8080").expect("Failed to connect to localhost on port 8080");
+    let tcp_stream_for_read = tcp_stream.try_clone().unwrap();
+    let _ = thread::spawn(move || {
+        read_from_stream(&tcp_stream_for_read);
+    });
 
-        tcp_stream.write(&buffer).unwrap();
+    loop {
+        let mut buf_writer = BufWriter::new(&tcp_stream);
+        let mut string_buffer = String::new();
+        stdin().read_line(&mut string_buffer).unwrap();
+        buf_writer.write(string_buffer.as_bytes()).unwrap();
+    }
+}
+
+fn read_from_stream(tcp_stream: &TcpStream) {
+    loop {
+        let mut string_buffer = String::new();
+        let mut buf_reader = BufReader::new(tcp_stream);
+        buf_reader.read_line(&mut string_buffer).unwrap();
+        print!("{}", string_buffer);
     }
 }
