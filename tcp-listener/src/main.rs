88ab1fd12eeb432022f00d66fe07a8a812feb339
Quack Quack <vivaladaman@gmail.com>
2021-10-22 15:14:27 +0700

    read and write simutaneously

--- tcp-listener/src/main.rs
+++ tcp-listener/src/main.rs
@@ -1,33 +1,38 @@
-use std::io::{BufRead, BufReader};
+use std::io::{stdin, BufRead, BufReader, BufWriter, Write};
 use std::net::TcpStream;
+use std::thread;
 use std::{net::TcpListener, panic::panic_any};
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                handle_tcp_request(&mut stream);
+            Ok(stream) => {
+                let stream_read = stream.try_clone().unwrap();
+                thread::spawn(move || handle_tcp_read(stream_read));
+
+                let stream_write = stream.try_clone().unwrap();
+                thread::spawn(move || handle_tcp_write(stream_write));
             }
             Err(err) => panic_any(err),
         }
     }
 }
 
-fn handle_tcp_request(stream: &mut TcpStream) {
-    println!("Connection established");
-    let mut read_stream = BufReader::new(stream);
-
+fn handle_tcp_read(stream: TcpStream) {
     loop {
         let mut string_buffer = String::new();
-        let receive_bytes = read_stream
-            .read_line(&mut string_buffer)
-            .expect("Error when reading stream");
+        let mut buf_reader = BufReader::new(&stream);
+        buf_reader.read_line(&mut string_buffer).unwrap();
+        print!("{}", string_buffer);
+    }
+}
 
-        if receive_bytes > 0 {
-            print!("{}", string_buffer);
-        } else {
-            println!("Connection ended");
-            return;
-        }
+fn handle_tcp_write(stream: TcpStream) {
+    loop {
+        let mut string_buffer = String::new();
+        stdin().read_line(&mut string_buffer).unwrap();
+
+        let mut buf_writer = BufWriter::new(&stream);
+        buf_writer.write(string_buffer.as_bytes()).unwrap();
     }
 }
